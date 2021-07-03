fb449a3fbdd28d60bc71abab817a3c88ae4970a3
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-03 10:43:25 +0100

    create video player component

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="video-player">
+    Welcome to VueTube
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoPlayer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
