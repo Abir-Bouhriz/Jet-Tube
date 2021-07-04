9364c8e63b448568aaeedeb5ded105951c1e738f
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-04 16:03:25 +0100

    add navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,36 @@
 <template>
-  <v-card>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant.sync="mini"
       permanent
     >
       <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-title>John Leider</v-list-item-title>
-
-        <v-btn
-          icon
-          @click.stop="mini = !mini"
-        >
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
+        <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        @click.stop="mini = !mini" />
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list dense>
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list>
         <v-list-item
           v-for="item in items"
           :key="item.title"
@@ -38,7 +46,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-  </v-card>
 </template>
 
 <script>
@@ -47,8 +54,8 @@ export default {
     drawer: false,
         items: [
           { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
+          { title: 'Explore', icon: 'mdi-compass' },
+          { title: 'Subscriptions', icon: 'mdi-youtube-subscription' },
         ],
         mini: true,
   }),
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <Navbar />
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import Navbar from '../components/Navbar'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
+      Navbar,
     },
   }
 </script>
