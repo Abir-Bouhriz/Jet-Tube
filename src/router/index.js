14a5977bba32c212466af70a1028d2bca6530da6
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-06 15:44:10 +0100

    add watch page to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Watch from '../components/Watch'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/watch',
+    name: 'Watch',
+    component: Watch
   },
   {
     path: '/about',
