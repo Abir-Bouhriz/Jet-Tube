3ae5e6213475fea239e1265529bb85be4f90d4c6
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-13 09:29:21 +0100

    change video info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -72,12 +72,20 @@
   data: () => ({
     loading: true,
     video: {
-      url: '/videoPlay',
-      thumb: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'Ellie Goulding - Burn (Official Video)',
-      views: '1.4 B',
-      createdAt: '7 years ago'
-    },
+    id: 1,
+    url: '/videoPlay',
+    title: "18-core iMac Pro Review: Not a Trap!",
+    thumbnail:
+      "https://i.ytimg.com/vi/jn9mHzXJIV0/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvJvk4k_UNB9nst4pFP-txM1TLZA",
+    youtubeURL: "https://www.youtube.com/embed/jn9mHzXJIV0",
+    creator: "Marques Brownlee",
+    likes: 0,
+    createdAt: '6 hours ago',
+    subscribers: '100k',
+    views: '1.4 B',
+    description:
+          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa vel inventore voluptatum reiciendis delectus quibusdam incidunt consequuntur, nostrum aperiam, natus quidem qui corrupti reprehenderit quaerat neque voluptatibus? Ullam, maiores temporibus! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa vel inventore voluptatum reiciendis delectus, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa vel inventore voluptatum reiciendis delectus'
+  },
     channel: {
       url: '/channel',
       avatar: 'profile.jpg'
