e69bdd936b608c03ae05f03509c563de521e60ea
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-13 16:25:09 +0100

    fix cards and make them flat

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,16 +1,14 @@
 <template>
   <v-card
-    class="content-bg card mx-auto"
-    :max-width="card.maxWidth"
+    class="content-bg card mx-auto pa-1 transparent"
     flat
-    tile
-    router
+    :max-width="card.maxWidth"
     :to="video.url"
   >
     <v-img :src="video.thumbnail" height="200px"></v-img>
     <v-row no-gutters>
       <v-col cols="2" v-if="card.type != 'noAvatar'">
-        <v-list-item class="pl-0 pt-3" router :to="channel.url">
+        <v-list-item class="pl-0 pt-3" :to="channel.url">
           <v-list-item-avatar color="grey darken-3">
             <v-img class="elevation-6" :src="channel.avatar"></v-img>
           </v-list-item-avatar>
