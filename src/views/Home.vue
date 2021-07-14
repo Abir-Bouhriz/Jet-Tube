0065eaba088b84bcb2652b6eb2eae4f891e22322
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-14 10:49:49 +0100

    get rid of comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,43 +19,6 @@
               :video="video"
               :channel="channel"
             ></video-card>
-            <!-- <v-card
-              class="content-bg card mx-auto"
-              max-width="350"
-              flat
-              tile
-              router
-              to="/watch/12"
-            >
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                height="200px"
-              ></v-img>
-              <v-row no-gutters>
-                <v-col cols="2">
-                  <v-list-item class="pl-0 pt-3" router to="/channels/12">
-                    <v-list-item-avatar color="grey darken-3">
-                      <v-img
-                        class="elevation-6"
-                        src="https://randomuser.me/api/portraits/men/1.jpg"
-                      ></v-img>
-                    </v-list-item-avatar>
-                  </v-list-item>
-                </v-col>
-                <v-col>
-                  <v-card-title class="pl-2 pt-3 subtitle-1 font-weight-bold">
-                    Top western road trips
-                  </v-card-title>
-
-                  <v-card-subtitle class="pl-2 pb-0">
-                    1,000 miles of wonder
-                  </v-card-subtitle>
-                  <v-card-subtitle class="pl-2 pt-0">
-                    9.6k views<v-icon>mdi-circle-small</v-icon>6 hours ago
-                  </v-card-subtitle>
-                </v-col>
-              </v-row>
-            </v-card> -->
           </v-skeleton-loader>
         </v-col>
       </v-row>
