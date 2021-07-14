31853b1126ee15c6b92733ca4a691b620dac125f
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2021-07-14 10:48:30 +0100

    grt rid of comments

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -75,29 +75,6 @@
                         :video="video"
                         :channel="channel"
                       ></video-card>
-                      <!-- <v-card class="content-bg card" max-width="250" flat tile>
-                        <v-img
-                          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                          height="200px"
-                        ></v-img>
-                        <v-row no-gutters>
-                          <v-col>
-                            <v-card-title
-                              class="pl-2 pt-3 subtitle-1 font-weight-bold"
-                            >
-                              Top western road trips
-                            </v-card-title>
-
-                            <v-card-subtitle class="pl-2 pb-0">
-                              1,000 miles of wonder
-                            </v-card-subtitle>
-                            <v-card-subtitle class="pl-2 pt-0">
-                              9.6k views<v-icon>mdi-circle-small</v-icon>6 hours
-                              ago
-                            </v-card-subtitle>
-                          </v-col>
-                        </v-row>
-                      </v-card> -->
                     </v-skeleton-loader>
                   </v-slide-item>
                 </v-slide-group>
@@ -122,48 +99,9 @@
                         :video="video"
                         :channel="channel"
                       ></video-card>
-                      <!-- <v-card
-                        class="content-bg card mx-auto"
-                        max-width="350"
-                        flat
-                        tile
-                      >
-                        <v-img
-                          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                          height="200px"
-                        ></v-img>
-                        <v-row no-gutters>
-                          <v-col cols="2">
-                            <v-list-item class="pl-0 pt-3">
-                              <v-list-item-avatar color="grey darken-3">
-                                <v-img
-                                  class="elevation-6"
-                                  src="https://randomuser.me/api/portraits/men/1.jpg"
-                                ></v-img>
-                              </v-list-item-avatar>
-                            </v-list-item>
-                          </v-col>
-                          <v-col>
-                            <v-card-title
-                              class="pl-2 pt-3 subtitle-1 font-weight-bold"
-                            >
-                              Top western road trips
-                            </v-card-title>
-
-                            <v-card-subtitle class="pl-2 pb-0">
-                              1,000 miles of wonder
-                            </v-card-subtitle>
-                            <v-card-subtitle class="pl-2 pt-0">
-                              9.6k views<v-icon>mdi-circle-small</v-icon>6 hours
-                              ago
-                            </v-card-subtitle>
-                          </v-col>
-                        </v-row>
-                      </v-card> -->
                     </v-skeleton-loader>
                   </v-col>
                 </v-row>
-                <v-card-text>home</v-card-text>
               </v-card>
             </v-tab-item>
           </v-tabs-items>
